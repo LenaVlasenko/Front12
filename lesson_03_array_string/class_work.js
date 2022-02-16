ba60edfb07570501a269d1b342498a25dc73d880
Elena <96632502+LenaVlasenko@users.noreply.github.com>
2022-02-16 16:14:30 +0200

    git commit -m

---
+++ lesson_03_array_string/class_work.js
@@ -0,0 +1,14 @@
+let result = document.getElementById('result');
+
+document.getElementById('btnStart').onclick = function (){
+    result.innerText = 'Пользователь щелкнул на кнопку'
+    let userNumber = document.getElementById('userNumber').value
+    console.log('Пользователь ввел' + userNumber)
+    if (userNumber < 18) {
+        result.innerText = ' Сайт для тех кому больше 18'
+    } else if (userNumber > 65) {
+        result.innerText = 'Вы слишком стары для интернета'
+    } else {
+        result.innerText = 'Добро пожаловать'
+    }
+}
