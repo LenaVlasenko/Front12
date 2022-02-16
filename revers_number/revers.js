72302f8d9871f2e32a8ba582852f87c1df402634
Elena <96632502+LenaVlasenko@users.noreply.github.com>
2022-02-16 21:50:26 +0200

    revers2

--- revers_number/revers.js
+++ revers_number/revers.js
@@ -1,16 +1,30 @@
 let divResult = document.getElementById('result')
 
+//математическая версия решения задачи
+
 document.getElementById('btnStart').onclick = function () {
     let userNumber = document.getElementById('userNumber').value
     console.log('Пользователь ввел:' + userNumber)
     //определяем временную переменную
     let tmpNumber = userNumber
     let strResult = ''
-    while (tmpNumber > 10) {
+    while (tmpNumber >= 10) {
         strResult += tmpNumber % 10 + ''
         tmpNumber = Math.floor(tmpNumber / 10)
     }
     strResult += tmpNumber
     console.log(strResult)
     divResult.innerText = strResult
+
+
+    console.log('--------------')
+//строковая версия решения задачи
+    let tmpString = userNumber.toString()
+    for (let i = 0; i < tmpString.length; i = i + 1) {
+        console.log (tmpString[i])
+    }
+    console.log('--------------')
+    for (let i = tmpString.length - 1; i >= 0; i = i - 1) {
+        console.log (tmpString[i])
+    }
 }
